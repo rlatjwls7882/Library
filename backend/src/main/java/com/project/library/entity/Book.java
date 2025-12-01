1bb310dc51b7fb653b0cbe97a05cbf6bd8cf8070
Seojin Kim <rlatjwls7882@gmail.com>
2025-12-01 22:46:14 +0900

    fix

--- backend/src/main/java/com/project/library/entity/Book.java
+++ backend/src/main/java/com/project/library/entity/Book.java
@@ -2,8 +2,6 @@ package com.project.library.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
